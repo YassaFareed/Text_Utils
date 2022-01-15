9ed8938a5e1083cd7532cc6de128552d036e2e74
YassaFareed <yassa.fareed99@gmail.com>
2022-01-15 10:26:33 -0800

    all operations working

--- mysite/views.py
+++ mysite/views.py
@@ -12,11 +12,13 @@ def analyze(request):
     #Get the text
     djtext = request.POST.get('text', 'default')
 
+
     # Check checkbox values
     removepunc = request.POST.get('removepunc', 'off')
     fullcaps = request.POST.get('fullcaps', 'off')
     newlineremover = request.POST.get('newlineremover', 'off')
     extraspaceremover = request.POST.get('extraspaceremover', 'off')
+
 
     #Check which checkbox is on
     if removepunc == "on":
@@ -25,50 +27,43 @@ def analyze(request):
         for char in djtext:
             if char not in punctuations:
                 analyzed = analyzed + char
-        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
 
-    elif(fullcaps=="on"):
+        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+
+    if(fullcaps=="on"):
         analyzed = ""
         for char in djtext:
             analyzed = analyzed + char.upper()
 
         params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        djtext = analyzed
         # Analyze the text
-        return render(request, 'analyze.html', params)
+        # return render(request, 'analyze.html', params)
 
-    elif(extraspaceremover=="on"):
+    if(extraspaceremover=="on"):
         analyzed = ""
         for index, char in enumerate(djtext):
             if not(djtext[index] == " " and djtext[index+1]==" "):
                 analyzed = analyzed + char
 
         params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        djtext = analyzed
         # Analyze the text
-        return render(request, 'analyze.html', params)
+        # return render(request, 'analyze.html', params)
 
-    elif (newlineremover == "on"):
+    if (newlineremover == "on"):
         analyzed = ""
         for char in djtext:
-            if char != "\n" and char != "\r":
+            if char != "\n" and char!="\r":
                 analyzed = analyzed + char
-
+            else:
+                print("no")
+        print("pre", analyzed)
         params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
-        # Analyze the text
-        return render(request, 'analyze.html', params)
-    else:
-        return HttpResponse("Error")
 
+    if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on"):
+        return HttpResponse("please select any operation and try again")
 
-# def capfirst(request):
-#     return HttpResponse("capitalize first")
-
-# def newlineremove(request):
-#     return HttpResponse("capitalize first")
-
-
-# def spaceremove(request):
-#     return HttpResponse("space remover")
-
-# def charcount(request):
-#     return HttpResponse("charcount ")
+    return render(request, 'analyze.html', params)
